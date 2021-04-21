213aa30f6dcfe200b87ad18b006c33490525a5a9
Asher <51806917+asher-lab@users.noreply.github.com>
2021-04-21 14:44:39 +0800

    Update Daily_Archive.sh

--- Daily_Archive.sh
+++ Daily_Archive.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 #
+# sudo ./Daily_archive.sh
 #
-# Daily_Archive - Archive Designated files and directories
-#############################################################
 #
-# Gather The Current Date
+
+
+# Daily_Archive - Archive designated files & directories
+########################################################
+#
+# Gather Current Date
 #
 DATE=$(date +%y%m%d)
 #
@@ -17,54 +21,54 @@ FILE=archive$DATE.tar.gz
 CONFIG_FILE=/archive/Files_To_Backup
 DESTINATION=/archive/$FILE
 #
-##################### Main Script ##########################
+######### Main Script #########################
 #
-# Check Backup Config File Exists
+# Check Backup Config file exists
 #
-if [ -f $CONFIG_FILE ] # make sure config file exists
-then # if it exists do nothing and continue on!
+if [ -f $CONFIG_FILE ] # Make sure the config file still exists.
+then 		# If it exists, do nothing but continue on.
 echo
-else
-echo
-echo "$CONFIG_FILE does not exist."
-echo "backup not completed due to missing configuration file"
-echo
-exit
+else 		# If it doesn't exist, issue error & exit script.
+	echo
+	echo "$CONFIG_FILE does not exist."
+	echo "Backup not completed due to missing Configuration File"
+	echo
+	exit
 fi
 #
-# Build the names of the files to backup
+# Build the names of all the files to backup
 #
-FILE_NO=1 # start on line 1 on the config file
+FILE_NO=1	    # Start on Line 1 of Config File.
+exec < $CONFIG_FILE # Redirect Std Input to name of Config File
 #
-exec < $CONFIG_FILE #Redirect std Input to name of config file
+read FILE_NAME 	    # Read 1st record
 #
-read FILE_NAME # read 1st record
-#
-while [ $? -eq 0 ] # create list of files to backup
+while [ $? -eq 0 ]  # Create list of files to backup
 do
-if [ -f $FILE_NAME -O -d $FILE_NAME ] #check the file exists
-then
-# if file exists add its name to list.
-FILE_LIST="$FILE_LIST $FILE_NAME"
-else
-# If files doesn't exist, issue warning
-echo
-echo $FILE_NAME, does not exist."
-echo "Obviously I will not include it in this archiuve."
-echo "It is listed on line $FILE_NO of the config file."
-echo "Continuing to build archive list."
-echo
+	# Make sure the file or directory exists.
+	if [ -f $FILE_NAME -o -d $FILE_NAME ]
+	then
+		# If file exists, add its name to the list.
+		FILE_LIST="$FILE_LIST $FILE_NAME"
+	else
+		# If file doesn't exist, issue warning
+	echo
+	echo "$FILE_NAME, does not exist."
+	echo "Obviously, I will not include it in this archive."
+	echo "It is listed on line $FILE_NO of the config file."
+	echo "Continuing to build archive list..."
+	echo
 fi
 #
-FILE_NO=$($File_NO +1] # Increase Line/File by one.
-Read FILE_NAME # read next record
+FILE_NO=$[$FILE_NO + 1] # Increase Line/File number by one.
+read FILE_NAME		# Read next record
 done
 #
-#######################################################################
+#######################################
 #
-# Backup the files and compress the archive
+# Backup the files and Compress Archive
 #
-echo "Starting Archive..."
+echo "Starting archive..."
 echo
 #
 tar -czf $DESTINATION $FILE_LIST 2> /dev/null
